4c31f01710afe8c2a5a35f51c4d1452975980edc
JonyLi <wsku1943lll@163.com>
2018-08-26 20:50:00 +0800

    Collapse

--- src/components/sidemenu.vue
+++ src/components/sidemenu.vue
@@ -1,27 +1,28 @@
 <template>
-    <el-aside width="170px" class="sidemenu" >
-        <el-menu v-if="menu_data.length>0" :collapse="isCollapse">
+    <el-aside :width="menu_width" :class="{sidemenu:true}" >
+        <el-menu v-if="menu_data.length>0" :collapse="isCollapse" router>
             <template v-for="menu in menu_data" class="menu_main">
                 <el-submenu v-if="menu.hasSub" index="menu.name" :key="menu.name">
                     <template slot="title"><i :class="menu.logo"></i><span slot="title">{{menu.name}}</span></template> 
                     <template v-for="sub in menu.sub" :index="sub.name" class="menu_sec">
                         <el-submenu v-if="sub.hasSub" index="sub.name" :key="sub.name">
                             <template slot="title"><i :class="sub.logo"></i>{{sub.name}}</template> 
-                            <el-menu-item v-for="end in sub.sub" :key="end.name" index="end.name">
+                            <el-menu-item v-for="end in sub.sub" :key="end.name" index="end.name" :route="end.link">
                                 <template slot="title"><i :class="end.logo"></i>{{end.name}}</template>
                             </el-menu-item>
                         </el-submenu>
-                        <el-menu-item v-if="!sub.hasSub" index="sub.name" :key="sub.name">
+                        <el-menu-item v-if="!sub.hasSub" index="sub.name" :key="sub.name" :route="sub.link">
                             <template slot="title"><i :class="sub.logo"></i>{{sub.name}}</template>
                         </el-menu-item>
                     </template>
                 </el-submenu>
-                <el-menu-item v-else index="menu.name" :key="menu.name">
+                <el-menu-item v-else index="menu.name" :key="menu.name" :route="menu.link">
                     <i :class="menu.logo"></i><span slot="title">{{menu.name}}</span>
                 </el-menu-item>
             </template>
             <!-- 收起菜单按钮 -->
-            <i class="el-icon-arrow-left" @click="collapse"/>
+            <!-- <i class="el-icon-arrow-left Collapse" @click="collapse"/> -->
+            <el-button :icon="CollapseIcon" round size="mini" @click="collapse" :class="{Collapse:true,Collapsed:Collapsed,unCollapse:unCollapse}">{{CollapseText}}</el-button>
         </el-menu>
     </el-aside>
 </template>
@@ -33,13 +34,23 @@
       data() {
         return {
           isCollapse: false,
+          CollapseIcon:"el-icon-arrow-left",
+          CollapseText:"收起",
+          Collapsed:false,
+          unCollapse:true,
+          menu_width:"170px",
           menu_data:menuData
         };
       },
       methods: {
         collapse(){
             this.isCollapse=!this.isCollapse;
-        }
+            this.CollapseIcon=this.CollapseIcon==="el-icon-arrow-left"?"el-icon-arrow-right":"el-icon-arrow-left";
+            this.CollapseText=this.CollapseText==="收起"?"展开":"收起"
+            this.menu_width=this.menu_width==="170px"?"65px":"170px";
+            this.Collapsed=!this.Collapsed;
+            this.unCollapse=!this.unCollapse;
+            }
         }
     }
 </script>
@@ -48,5 +59,15 @@
     .el-menu{
         height: calc(100vh - 50px);
         font-size: 16px;
+    }
+    .Collapse{
+        float:right;
+        margin-right: -10px;
+    }
+    .Collapsed{
+
+    }
+    .unCollapse{
+
     }
 </style>
--- src/store/user.js
+++ src/store/user.js
@@ -1,20 +1,20 @@
 const user = {
-    state:{
-        isLogin:true,
-        token:'',
-        userId:'',
-        userName:'',
-        authorities:[]
+    state: {
+        isLogin: false,
+        token: '',
+        userId: '',
+        userName: '',
+        authorities: []
     },
-    mutations:{
-        login(state,payload){
+    mutations: {
+        login(state, payload) {
             // console.log('login mutation');
             console.log(payload);
-            state.isLogin=true;
-            state.token=payload['data']['token'];
-            state.userId=payload['data']['user_id'];
-            state.userName=payload['data']['username'];
-            state.authorities=payload['data']['authorities'];
+            state.isLogin = true;
+            state.token = payload['data']['token'];
+            state.userId = payload['data']['user_id'];
+            state.userName = payload['data']['username'];
+            state.authorities = payload['data']['authorities'];
         }
     }
 };
