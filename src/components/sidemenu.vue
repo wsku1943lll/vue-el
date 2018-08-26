a166d55cafc692a1837c26b708354bb9e0e8832a
JonyLi <wsku1943lll@163.com>
2018-08-26 13:39:46 +0800

    2

--- src/components/sidemenu.vue
+++ src/components/sidemenu.vue
@@ -1,11 +1,30 @@
 <template>
     <el-aside width="200px" class="sidemenu" >
-        <el-menu >
-            <el-submenu index="1">
-                <template slot="title"><i class="el-icon-message"></i>导航一</template>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
+        <el-menu v-if="menu_data.length>0">
+            <div v-for="menu in menu_data" :key="menu.name">
+                <el-submenu v-if="menu.hasSub" index="menu.name" :key="menu.name">
+                    <template slot="title"><i :class="menu.logo"></i>{{menu.name}}</template> 
+                    <div v-for="sub in menu.sub" :index="sub.name" :key="sub.name" >
+                        <el-submenu v-if="sub.hasSub" index="sub.name" v-for="end in sub.sub" :key="end.name">
+                        </el-submenu>
+                        <el-menu-item v-else index="sub.name">
+                            <template slot="title"><i :class="sub.logo"></i>{{sub.name}}</template>
+                        </el-menu-item>
+                    </div>
+                </el-submenu>
+                <el-menu-item v-else index="menu.name">
+                    <template slot="title"><i :class="menu.logo"></i>{{menu.name}}</template>
+                </el-menu-item>
+            </div>
+            <!-- <el-submenu v-for="menu in menu_data" :index="menu.name" :key="menu.name" :menu-data="menu">
+                <template slot="title"><i :class="menu.logo"></i>{{menu.name}}</template>
+                <el-menu-item index="1-1">选项1</el-menu-item> -->
+                <!-- <el-menu-item index="1-2">选项2</el-menu-item>
+                <el-submenu v-if="menu.sub.length>1" >
+                    <el-menu-item index="sub.name" v-for="sub in menu.sub" :key="sub.name">{{sub.name}}</el-menu-item>
+                </el-submenu>
             </el-submenu>
+            <div>
             <el-submenu index="2">
                 <template slot="title"><i class="el-icon-menu"></i>导航二</template>
                 <el-menu-item-group>
@@ -21,16 +40,19 @@
                     <el-menu-item index="2-4-1">选项4-1</el-menu-item>
                 </el-submenu>
             </el-submenu>
+            </div> -->
         </el-menu>
     </el-aside>
 </template>
 
 <script>
+    import menuData from '../store/menu';
     export default {
         name: "sidemenu",
       data() {
         return {
-          isCollapse: true
+          isCollapse: true,
+          menu_data:menuData
         };
       },
       methods: {
---
+++ src/store/menu.js
@@ -0,0 +1,11 @@
+export default [{
+        name: "菜单一",
+        logo: "el-icon-message",
+        hasSub: true,
+        link: "#",
+        sub: [{ name: '小咖', hasSub: false, link: "#", sub: [] },
+            { name: '大咖', hasSub: false, link: "#", sub: [] }
+        ]
+    },
+    { name: '菜单二', logo: "el-icon-menu", hasSub: false, link: "#", sub: [] }
+];
